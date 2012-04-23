bbe10ac2cd833369b9485ae652284e61572791de
Stefan Jansen <stefan_j@live.nl>
2012-04-23 13:42:45 +0200

    Introduced makefile.

--- service/main/calculator.cc
+++ service/main/calculator.cc
@@ -1,5 +1,10 @@
 #include "include/calculator.h"
 
+/**
+* Calculates the sum of two integers.
+* @param a Integer A.
+* @param b Integer B.
+**/
 int Calculator::sum(int a, int b) {
 	return a + b;
 }
