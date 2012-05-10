ba867dcef20aa5e0998f240b34dc82d7e35a943a
Hanna Hermsen <hannahermsen@gmail.com>
2012-05-10 12:07:38 +0200

    laatste versie van hanna's simulatordingetjes voor iteratie 3

--- simulator/test/StateMachineTest.cpp
+++ simulator/test/StateMachineTest.cpp
@@ -16,17 +16,3 @@ TEST(StateMachineTest, testStateMachineInit) {
 	fsm.init();
 	EXPECT_TRUE(fsm.init());
 }
-
-TEST (StateMachineTest, testRunStateMachine) {
-	int argc = 4;
-	char *argv[argc];
-	argv[0] = (char*)"simulator"; argv[1] = (char*)"127.0.0.1"; argv[2] = (char*)"5000"; argv[3] = (char*)"10";
-
-	ifstream stream("/home/hanna/KeyTech/simulator/test/open.txt");
-
-	if (stream.is_open())	cout << "file opened" << endl;
-	else					cout << "file not opened";
-
-	StateMachine sm(argc, argv, &stream);
-	sm.runStateMachine();
-}
